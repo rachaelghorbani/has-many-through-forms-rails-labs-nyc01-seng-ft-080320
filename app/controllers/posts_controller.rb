95a038da43e406e3f2729b9dc2eefb357e95d660
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-31 11:11:36 -0400

    confused

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+    before_action :find_post, only:[:show]
+
   def show
-    @post = Post.find(params[:id])
+   
   end
 
   def index
@@ -9,16 +11,26 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    if @post.valid?
+    redirect_to post_path(@post)
+    else 
+        render :new
+    end
   end
 
   private
+    def find_post
+        @post = Post.find(params[:id])
+    end
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
+
+
 end
